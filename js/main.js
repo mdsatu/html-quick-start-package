5a1f4043dcab8f977290fb9a6d8baab8d5a6a1c4
ndsatu <mdsatu.pro@gmail.com>
2020-07-01 14:33:52 +0600

    Added cppyright

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+// ============================
+// Template Name: HTML QS
+// Developer: MD Satu
+// Developed By: www.mdsatu.com
+// ============================
+
 $(document).ready(function(){
 
 	// Scroll to Up
